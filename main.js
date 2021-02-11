9b504ea2100f7f2f603d97149deaf54932082cba
Ginver <74971813+Ginver@users.noreply.github.com>
2021-02-11 20:26:04 +0100

    setup & opdracht 1 gemaakt

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+const Button = document.getElementById('searchButton')
+Button.addEventListener("click",() => {
+});
+let country = 'Belgium';
+const url = https://restcountries.eu/rest/v2/name/{name}
+    async function searchByCountry() {
+            const response = await axios.get(
+                https://restcountries.eu/#api-endpoints-language//
+        )
+
+   };
+    console.log(country);
