4982a9c14d886fe3fdd54f51d41b9732df1a21f2
Ginver <74971813+Ginver@users.noreply.github.com>
2021-02-14 16:58:21 +0100

    Belgie wordt opgehaald

--- main.js
+++ main.js
@@ -1,14 +1,80 @@
+// Odracht 1.
+// ## 1/1. De gebruiker kan de knop zien
+// - [x] Zoek knop maken (HTML)
+// - [x] id meegeven -> om met javascript te selecteren (HTML)
 
+const button = document.getElementById('searchButton');
+//console.log(button);
 
-const Button = document.getElementById('searchButton')
-Button.addEventListener("click",() => {
-});
-let country = 'Belgium';
-const url = https://restcountries.eu/rest/v2/name/{name}
+// ## 1/2. De gebruiker gaat klikken
+// - [x] Knop selecteren (getElementById, opslaan in variabele)
+// - [x] Event listener & Event Handler toevoegen aan knop (addEventListener, click, async functie)
+
+button.addEventListener("click", searchByCountry);
+
+// ## 1/3. Wanneer de gebruiker klikt wordt mijn async function aangeroepen
+// - [x] Variable met maken country -> "Belgie" (hardcoden)
+//     - [x] Variabele Url maken -> https://restcountries.eu/rest/v2/name/${country}?fullText=true
+// - [x] axios.get(url)
+// - [x] await toevoegen
+// - [x] response -> checken
+
     async function searchByCountry() {
-            const response = await axios.get(
-                https://restcountries.eu/#api-endpoints-language//
-        )
+            console.log("GET COUNTRY HERE");
+        const country = "Belgium";
+        const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
+        const response = await axios.get(url);
+            console.log(response); // -> country
+        // je krijgt een array - niet een object
+        //
+        const countryData = response.data[0];
+            console.log(response.data[0].name);
 
-   };
-    console.log(country);
+// Opdracht 2.
+// Maak op basis van de response de volgende string en log dit in de console: `[country-naam] is situated in [subarea-name]. It has a population of [amount] people.`
+// - [ ] Goed in de data kijken, waar zit deze info: -> loggen
+// - [ ] variabelen maken: countryName, countryArea, countryPopulation
+// - [ ] template string maken met -> `${countryName} ... etc`
+
+// const geography = `${countryData.name} is situated in ${countryData.subregion}. It has a population of ${countryData.currency} people.`
+// const capital = `The capital is ${countryData.capital}`
+     };
+
+// 3. Maak op basis van de response de volgende string en log dit in de console: `The capital is [city]`
+// - [ ] Goed in de data kijken, waar zit deze info: -> loggen
+// - [ ] variabelen maken: countryCapital
+// - [ ] template string maken met -> `${countryCapital} ... etc`
+
+
+
+// Opdracht 4. Maak een functie die ongeacht het aantal currencies die in een land gebruikt worden, een string maakt. In een land kunnen één of twee currencies gebruikt worden:
+// 1 valuta: `and you can pay with [currency]'s`
+// 2 valuta's: `and you can pay with [currency]'s and [currency]'s`
+//
+// - [ ] Goed in de data kijken, waar zit deze info: -> loggen
+// - [ ] variabele maken, en de currencies uit de data daaraan toekennen
+// - [ ] functie maken -> formatCurrencies
+//     - [ ] aanroepen -> logje in de functie zetten om te checken
+// - [ ] input: currencies, parameter(s) toevoegen, argument(en) meegeven
+// - [ ] parameter loggen om te kijken of de input hebben
+// - [ ] if
+//     - [ ] 1 currency -> `and you can pay with [currency]'s`
+//     - [ ] 2 currency -> `and you can pay with [currency]'s and [currency]'s`
+//     - [ ] return de waarde `and you can pay with [currency]'s and [currency]'s`
+//     console.log(response) // countries!!
+
+//
+// console.log(currencies)
+// //hier definieren
+// //hier aanroepen -> currenties is in scope
+// formatCurrencies(currencies) // argument -> waarde
+// //console.log(currencies)
+// };
+// const button = document.getElementById('searchButton')
+// button.addEventListener("click", searchByCountry);
+//
+// //hier definieren
+// function formatCurrencies(currencyArray){ //parameter -> placeholder
+//     console.log('INPUT', currencyArray)
+
+// Opdracht 8
