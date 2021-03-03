ff9e6af2e01de9f1d0e113ee72be7fb37526287e
Ginver <74971813+Ginver@users.noreply.github.com>
2021-03-03 15:40:28 +0100

    some changes have been made

--- main.js
+++ main.js
@@ -17,30 +17,33 @@ button.addEventListener("click", clearSearchField);
         // console.log(inputElement);
         const userInput = inputElement.value;
         // console.log("UserINput:", userInput);
-
+        try {
         const country = userInput;
         const url = `https://restcountries.eu/rest/v2/name/${country}?fullText=true`;
-        try {
+
             const response = await axios.get(url);
             console.log(response); // -> countries!
             const countryData = response.data[0];
             const countryFlag = countryData.flag;
             console.log(countryFlag);
 
-            const geography = `${countryData.name} is situated in ${countryData.subregion}. It has a population of ${countryData.population} people.`;
+            const geography = `${countryData.name} is situated in ${countryData.subregion}.`
             //console.log(geography);
+
+            const population = `It has a population of ${countryData.population} people.`;
+            //console.log(population);
 
             const countryCapital = `The capital is ${countryData.capital} `;
             //console.log(countryCapital);
 
-            const currency = `and you can pay with ${countryData.currencies[0][1][2].name}'s.`;
+            const currency = `and you can pay with ${countryData.currencies[0].name}'s.`;
             //console.log(currency);
 
             const spokenLanguage = `They speak ${countryData.languages[0].name}.`
             ///console.log(spokenLanguage);
 
             const countryInfo = document.getElementById('countryInfo');
-            countryInfo.textContent = geography + ' ' + countryCapital + ' ' + currency + ' ' + spokenLanguage;
+            countryInfo.textContent = geography + ' ' + population + ' ' + countryCapital + ' ' + currency + ' ' + spokenLanguage;
             document.getElementById('flagImage').src = countryFlag;
             // imageFlag.src = countryFlag;
 
